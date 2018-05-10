3494c1f0f5c1809365c2515345a691c093aa85df
Nick <myfjdthink@163.com>
2018-05-10 10:40:50 +0800

    调整代码结构

--- src/patch/HttpServer.ts
+++ src/patch/HttpServer.ts
@@ -14,23 +14,31 @@ export class KlgHttpServerPatcher extends HttpServerPatcher {
     super(options)
   }
 
-  wrapResponse (options, response, tracer, span) {
+  /**
+   * 放在 class 这一层，方便子类替换实现
+   * @param options
+   * @param req
+   * @param res
+   * @param tracer
+   * @param span
+   */
+  recordResponse (options, req, res, tracer, span) {
     const traceManager = this.getTraceManager()
     const shimmer = this.getShimmer()
 
-    // 为了记录 response 参数 见 http://nodejs.cn/api/http.html#http_response_end_data_encoding_callback
-    shimmer.wrap(response, 'write', function responseWriteWrapper (write) {
+    // 为了记录 res 参数 见 http://nodejs.cn/api/http.html#http_response_end_data_encoding_callback
+    shimmer.wrap(res, 'write', function responseWriteWrapper (write) {
       const bindRequestWrite = traceManager.bind(write)
       return function wrappedResponseWrite (chunk, encoding, callback) {
-        responseLog(response, chunk, encoding)
+        responseLog(res, chunk, encoding)
         return bindRequestWrite.apply(this, arguments)
       }
     })
 
-    shimmer.wrap(response, 'end', function responseWriteWrapper (write) {
+    shimmer.wrap(res, 'end', function responseWriteWrapper (write) {
       const bindRequestWrite = traceManager.bind(write)
       return function wrappedResponseWrite (chunk, encoding, callback) {
-        responseLog(response, chunk, encoding)
+        responseLog(res, chunk, encoding)
         return bindRequestWrite.apply(this, arguments)
       }
     })
@@ -65,6 +73,99 @@ export class KlgHttpServerPatcher extends HttpServerPatcher {
     }
   }
 
+  /**
+   * 放在 class 这一层，方便子类替换实现
+   * @param options
+   * @param req
+   * @param res
+   * @param tracer
+   * @param span
+   */
+  recordQuery (options, req, res, tracer, span) {
+    if (options.recordGetParams) {
+      const query = this.processGetParams(req)
+      span.log({
+        query
+      })
+    }
+  }
+
+  /**
+   * 放在 class 这一层，方便子类替换实现
+   * @param options
+   * @param req
+   * @param res
+   * @param tracer
+   * @param span
+   */
+  recordPostData (options, req, res, tracer, span) {
+    const self = this
+    const traceManager = this.getTraceManager()
+    const shimmer = this.getShimmer()
+    let chunks = []
+    if (options.recordPostData && req.method && req.method.toUpperCase() === 'POST') {
+      shimmer.wrap(req, 'emit', function wrapRequestEmit (emit) {
+        const bindRequestEmit = traceManager.bind(emit)
+
+        return function wrappedRequestEmit (this: IncomingMessage, event) {
+          if (event === 'data') {
+            const chunk = arguments[1] || []
+
+            chunks.push(chunk)
+          }
+
+          return bindRequestEmit.apply(this, arguments)
+        }
+      })
+    }
+
+    function onFinishedFactory (eventName) {
+      return function onFinished () {
+        res.removeListener('finish', onFinished)
+        req.removeListener('aborted', onFinished)
+        if (eventName !== 'aborted' && options.recordPostData && req.method && req.method.toUpperCase() === 'POST') {
+          const transformer = options.bufferTransformer || self.bufferTransformer
+          const postData = transformer(chunks)
+
+          span.log({
+            data: postData
+          })
+          // clear cache
+          chunks = []
+        }
+
+        span.setTag('http.aborted', {
+          type: 'bool',
+          value: eventName === 'aborted'
+        })
+
+        self.beforeFinish(span, res)
+        span.finish()
+        tracer.finish(options)
+        self.afterFinish(span, res)
+      }
+    }
+
+    res.once('finish', onFinishedFactory('finish'))
+    req.once('aborted', onFinishedFactory('aborted'))
+  }
+
+  /**
+   * 如果要新增钩子，复写此方法，添加调用
+   * super.wrapRequest()
+   * this.newRecord(options, req, res, tracer, span)
+   * @param options
+   * @param req
+   * @param res
+   * @param tracer
+   * @param span
+   */
+  wrapRequest (options, req, res, tracer, span) {
+    this.recordQuery(options, req, res, tracer, span)
+    this.recordPostData(options, req, res, tracer, span)
+    this.recordResponse(options, req, res, tracer, span)
+  }
+
   shimmer (options) {
     const self = this
     const traceManager = this.getTraceManager()
@@ -91,65 +192,10 @@ export class KlgHttpServerPatcher extends HttpServerPatcher {
             const tags = self.buildTags(req)
             const span = self.createSpan(tracer, tags)
 
-            if (options.recordGetParams) {
-              const query = self.processGetParams(req)
-
-              span.log({
-                query
-              })
-            }
-
-            let chunks = []
-            if (options.recordPostData && req.method && req.method.toUpperCase() === 'POST') {
-              shimmer.wrap(req, 'emit', function wrapRequestEmit (emit) {
-                const bindRequestEmit = traceManager.bind(emit)
-
-                return function wrappedRequestEmit (this: IncomingMessage, event) {
-                  if (event === 'data') {
-                    const chunk = arguments[1] || []
-
-                    chunks.push(chunk)
-                  }
-
-                  return bindRequestEmit.apply(this, arguments)
-                }
-              })
-            }
-            self.wrapResponse(options, res, tracer, span)
+            self.wrapRequest(options, req, res, tracer, span)
 
             tracer.named(`HTTP-${tags['http.method'].value}:${tags['http.url'].value}`)
             tracer.setCurrentSpan(span)
-
-            function onFinishedFactory (eventName) {
-              return function onFinished () {
-                res.removeListener('finish', onFinished)
-                req.removeListener('aborted', onFinished)
-
-                if (eventName !== 'aborted' && options.recordPostData && req.method && req.method.toUpperCase() === 'POST') {
-                  const transformer = options.bufferTransformer || self.bufferTransformer
-                  const postData = transformer(chunks)
-
-                  span.log({
-                    data: postData
-                  })
-                  // clear cache
-                  chunks = []
-                }
-
-                span.setTag('http.aborted', {
-                  type: 'bool',
-                  value: eventName === 'aborted'
-                })
-
-                self.beforeFinish(span, res)
-                span.finish()
-                tracer.finish(options)
-                self.afterFinish(span, res)
-              }
-            }
-
-            res.once('finish', onFinishedFactory('finish'))
-            req.once('aborted', onFinishedFactory('aborted'))
 
             return requestListener(req, res)
           })
